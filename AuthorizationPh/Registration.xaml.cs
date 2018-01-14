e6075e004d0898472e49f0e2f80b130592566618
UkiOnna <sikirovtemirlan@mail.ru>
2018-01-14 19:12:06 +0600

    d

---
+++ AuthorizationPh/Registration.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuthorizationPh
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        private MainWindow mainWindow;
+
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        public Registration(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void PhotoButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            //dialog.DefaultExt = ".png";
+            dialog.ShowDialog();
+
+            string filePath = dialog.FileName;
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(filePath);
+            image.EndInit();
+
+            imageContainer.Source = image;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (LoginText.Text == string.Empty||ParolText.Text==string.Empty||MailText.Text==string.Empty)
+            {
+                MessageBox.Show("Пожалуйства введите все данные");
+            }
+
+            else
+            {
+                mainWindow.Content = new SignPage(mainWindow,LoginText.Text,ParolText.Text);
+            }
+        }
+    }
+}
---
+++ AuthorizationPh/SignPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuthorizationPh
+{
+    /// <summary>
+    /// Логика взаимодействия для SignPage.xaml
+    /// </summary>
+    public partial class SignPage : Page
+    {
+        private MainWindow mainWindow;
+        private string _login;
+        private string _password;
+
+        public SignPage()
+        {
+            InitializeComponent();
+        }
+
+        public SignPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        public SignPage(MainWindow mainWindow,string login,string password)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+            _login = login;
+            _password = password;
+        }
+
+        private void RegistrationClick(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Content = new Registration(mainWindow);
+        }
+
+        private void SignClick(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == _login && Password.Text == _password)
+            {
+                mainWindow.Content = new Hello(_login);
+            }
+
+            else
+            {
+                MessageBox.Show("Вы ввели неправильный логин или пароль");
+            }
+        }
+    }
+}
